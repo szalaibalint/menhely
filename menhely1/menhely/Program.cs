c1e0a07720e080f52bef079f722496de05a8f6d8
szalaibalint <szalaibalint7521@gmail.com>
2024-06-24 12:28:01 +0200

    menhely1, menhely2

---
+++ menhely1/menhely/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menhely
+{
+    class Allat
+    {
+        private string nev;
+        private int szuletesiEv;
+
+        private int szepsegPont, viselkedesPont;
+        private int pontSzam;
+
+        private static int aktualisEv;
+        private static int korHatar;
+
+        public Allat(string nev, int szuletesiEv)
+        {
+            this.nev = nev;
+            this.szuletesiEv = szuletesiEv;
+        }
+
+        public int Kor()
+        {
+            return aktualisEv - szuletesiEv;
+        }
+
+        public void Pontozzak(int szepsegPont, int viselkedesPont)
+        {
+            this.szepsegPont = szepsegPont;
+            this.viselkedesPont = viselkedesPont;
+            if (Kor() <= korHatar)
+            {
+                pontSzam = viselkedesPont * Kor() + szepsegPont * (korHatar - Kor());
+            }
+            else
+            {
+                pontSzam = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return nev + " pontszama: " + pontSzam + " pont";
+        }
+
+        public string Nev
+        {
+            get { return nev; }
+        }
+
+        public int SzuletesiEv
+        {
+            get { return szuletesiEv; }
+        }
+
+        public int SzepsegPont
+        {
+            get { return szepsegPont; }
+        }
+
+        public int ViselkedesPont
+        {
+            get { return viselkedesPont; }
+        }
+
+        public int PontSzam
+        {
+            get { return pontSzam; }
+        }
+
+        public static int AktualisEv
+        {
+            get { return aktualisEv; }
+            set { aktualisEv = value; }
+        }
+
+        public static int KorHatar
+        {
+            get { return korHatar; }
+            set { korHatar = value; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Allat allat;
+
+            int aktEv = 2015, korhatar = 10;
+
+            string nev;
+            int szulEv;
+            int szepseg, viselkedes;
+
+            Allat.AktualisEv = aktEv;
+            Allat.KorHatar = korhatar;
+
+            nev = "Vakarcs";
+            szulEv = 2010;
+            szepseg = 5;
+            viselkedes = 3;
+
+            allat = new Allat(nev, szulEv);
+
+            allat.Pontozzak(szepseg, viselkedes);
+
+            Console.WriteLine(allat);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ menhely2/menhely/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menhely
+{
+    class Allat
+    {
+        private string nev;
+        private int szuletesiEv;
+        private int igazolasSzam;
+
+        private int rajtSzam;
+
+        private int szepsegPont, viselkedesPont;
+        private int pontSzam;
+
+        private static int aktualisEv;
+        private static int korHatar;
+
+        public Allat(string nev, int szuletesiEv, int igazolasSzam)
+        {
+            this.nev = nev;
+            this.szuletesiEv = szuletesiEv;
+            this.igazolasSzam = igazolasSzam;
+        }
+
+        public int Kor()
+        {
+            return aktualisEv - szuletesiEv;
+        }
+
+        public void Pontozzak(int szepsegPont, int viselkedesPont)
+        {
+            this.szepsegPont = szepsegPont;
+            this.viselkedesPont = viselkedesPont;
+            if (Kor() <= korHatar)
+            {
+                pontSzam = viselkedesPont * Kor() + szepsegPont * (korHatar - Kor());
+            }
+            else
+            {
+                pontSzam = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return nev + " pontszama: " + pontSzam + " pont,\nrajtszam: " + rajtSzam;
+        }
+
+        public string Nev
+        {
+            get { return nev; }
+        }
+
+        public int SzuletesiEv
+        {
+            get { return szuletesiEv; }
+        }
+
+        public int IgazolasSzam
+        {
+            get { return igazolasSzam; }
+        }
+
+
+        public int SzepsegPont
+        {
+            get { return szepsegPont; }
+        }
+
+        public int ViselkedesPont
+        {
+            get { return viselkedesPont; }
+        }
+
+        public int PontSzam
+        {
+            get { return pontSzam; }
+        }
+
+        public static int AktualisEv
+        {
+            get { return aktualisEv; }
+            set { aktualisEv = value; }
+        }
+
+        public int RajtSzam
+        {
+            get { return rajtSzam; }
+            set { rajtSzam = value; }
+        }
+
+        public static int KorHatar
+        {
+            get { return korHatar; }
+            set { korHatar = value; }
+        }
+    }
+
+    internal class Program
+    {
+        public static void AllatVerseny()
+        {
+            Allat allat;
+
+            string nev;
+            int szulEv;
+            int igazolasSzam;
+            int szepseg, viselkedes;
+            int veletlenPontHatar = 10;
+            int jelenlegiRajtszam = 1;
+
+            Random rand = new Random();
+
+            int osszesVersenyzo = 0;
+            int osszesPont = 0;
+            int legtobbPont = 0;
+
+            Console.WriteLine("Kezdődik a verseny");
+
+            char tovabb = 'i';
+            while (tovabb == 'i')
+            {
+                Console.Write("Az állat neve: ");
+                nev = Console.ReadLine();
+
+                Console.Write("Születési éve: ");
+                while (!int.TryParse(Console.ReadLine(), out szulEv)
+                    || szulEv <= 0
+                    || szulEv > Allat.AktualisEv)
+                {
+                    Console.WriteLine("Hibás adat, kérem újra.");
+                }
+
+                Console.Write("Igazolás száma: ");
+                while (!int.TryParse(Console.ReadLine(), out igazolasSzam)
+                    || igazolasSzam <= 0)
+                {
+                    Console.WriteLine("Hibás adat, kérem újra.");
+                }
+
+                szepseg = rand.Next(veletlenPontHatar + 1);
+                viselkedes = rand.Next(veletlenPontHatar + 1);
+
+                allat = new Allat(nev, szulEv, igazolasSzam);
+
+                allat.RajtSzam = jelenlegiRajtszam;
+                jelenlegiRajtszam++;
+
+                allat.Pontozzak(szepseg, viselkedes);
+
+                Console.WriteLine(allat);
+
+                osszesVersenyzo++;
+                osszesPont += allat.PontSzam;
+                if (legtobbPont < allat.PontSzam)
+                {
+                    legtobbPont = allat.PontSzam;
+                }
+
+                Console.Write("Van még állat (i/n) ");
+
+                tovabb = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nÖsszesen " + osszesVersenyzo + " versenyző volt,"
+                + "\nösszpontszámuk: " + osszesPont + " pont,"
+                + "\nlegnagyobb pontszám: " + legtobbPont + " pont,"
+                + "\nátlag pontszám: " + (osszesPont / osszesVersenyzo));
+        }
+
+
+        static void Main(string[] args)
+        {
+            int aktEv = 2015, korhatar = 10;
+
+            Allat.AktualisEv = aktEv;
+            Allat.KorHatar = korhatar;
+
+            AllatVerseny();
+
+            Console.ReadKey();
+        }
+    }
+}
